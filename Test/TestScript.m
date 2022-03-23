ce1ffa907930b93de3f9e0f3cf3a88a6d4000162
YukiKamatani <ykamatan@mathworks.com>
2022-03-24 03:58:25 +0900

    Update

--- Test/TestScript.m
+++
@@ -1,6 +0,0 @@
-import matlab.unittest.plugins.XMLPlugin;
-import matlab.unittest.TestRunner;
-suite = testsuite('TestFile.mldatx');
-runner = TestRunner.withTextOutput;
-runner.addPlugin(XMLPlugin.producingJUnitFormat('TestResult/myJUnitResults.xml'));
-runner.run(suite);
