65971bc034aebe637041d114612c690b95955132
YukiKamatani <ykamatan@mathworks.com>
2022-03-30 09:58:42 +0900

    NewTest

--- Test/TestScript.m
+++ Test/TestScript.m
@@ -1,12 +1,13 @@
   import matlab.unittest.TestRunner;
   import matlab.unittest.Verbosity;
   import matlab.unittest.plugins.XMLPlugin;
+  import sltest.plugins.TestManagerResultsPlugin;
   
   suite = testsuite(pwd, 'IncludeSubfolders', true);
   
   [~,~] = mkdir('TestResult');
   
-  runner = TestRunner.withTextOutput('OutputDetail', Verbosity.Detailed);
+  runner = TestRunner.withTextOutput;
   runner.addPlugin(XMLPlugin.producingJUnitFormat('TestResult/results.xml'));
   
   results = runner.run(suite);
